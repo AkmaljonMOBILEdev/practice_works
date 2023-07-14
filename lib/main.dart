b8d2825a5afc961f098d5bad00bc1ec767fa624a
Akmaljon <akmaljondeveloper@gmail.com>
2023-07-14 21:23:58 +0500

    valorant's first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,8 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:main_project/local/storage.dart';
 import 'package:main_project/ui/home_screen/home_screen.dart';
-import 'package:main_project/utils/theme.dart';
 
-Future<void> main()async{
-  WidgetsFlutterBinding.ensureInitialized();
-  StorageRepository.getInstance();
-  runApp(
-    EasyLocalization(
-        supportedLocales: const [
-          Locale('en','EN'),
-          Locale('ru','RU'),
-          Locale('uz','UZ')
-        ],
-        path: 'assets/translations',
-        fallbackLocale: const Locale('en', 'EN'),
-        child: const MyApp()),
-  );
+void main(){
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -25,16 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.light,
-      home: const HomeScreen(),
-
-      localizationsDelegates: context.localizationDelegates,
-      supportedLocales: context.supportedLocales,
-      locale: context.locale,
+      home: HomeScreen(),
     );
   }
 }
